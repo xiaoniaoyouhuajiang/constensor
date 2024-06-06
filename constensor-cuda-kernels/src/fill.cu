774fb4aaa01426009be65a4f0a35aceae1bbea86
EricLBuehler <ericlbuehler@gmail.com>
2024-06-06 18:02:26 -0400

    More fixes

--- constensor-cuda-kernels/src/fill.cu
+++ constensor-cuda-kernels/src/fill.cu
@@ -12,6 +12,7 @@ extern "C" __global__ void fill_u8(uint8_t *buf, uint8_t value, const size_t num
 extern "C" __global__ void fill_u32(uint32_t *buf, uint32_t value, const size_t numel) { fill_with(buf, value, numel); }
 extern "C" __global__ void fill_i64(int64_t *buf, int64_t value, const size_t numel) { fill_with(buf, value, numel); }
 extern "C" __global__ void fill_f32(float *buf, float value, const size_t numel) { fill_with(buf, value, numel); }
+extern "C" __global__ void fill_f64(double *buf, double value, const size_t numel) { fill_with(buf, value, numel); }
 
 #ifdef HALF
 #include "cuda_fp16.h"
